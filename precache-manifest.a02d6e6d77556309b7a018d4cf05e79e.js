c1b7080855cfe248b1d893ba23b5ed8cdad7d6e4
KingWatch99 <kinggames1239@gmail.com>
2021-08-12 00:32:09 +0200

    Updates

---
+++ precache-manifest.a02d6e6d77556309b7a018d4cf05e79e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903e195aaacff959dd82c145de36004c",
+    "url": "/KingWatch/index.html"
+  },
+  {
+    "revision": "608e3b9892e7acf4b645",
+    "url": "/KingWatch/static/css/main.96e165a8.chunk.css"
+  },
+  {
+    "revision": "2fe3ac8a1bcf250e739b",
+    "url": "/KingWatch/static/js/2.a95ed01d.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/KingWatch/static/js/2.a95ed01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608e3b9892e7acf4b645",
+    "url": "/KingWatch/static/js/main.3c82805e.chunk.js"
+  },
+  {
+    "revision": "9256139420ca6c3b260f",
+    "url": "/KingWatch/static/js/runtime-main.b70300e0.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/KingWatch/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/KingWatch/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/KingWatch/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/KingWatch/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/KingWatch/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/KingWatch/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/KingWatch/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/KingWatch/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/KingWatch/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/KingWatch/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/KingWatch/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/KingWatch/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/KingWatch/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/KingWatch/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/KingWatch/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
